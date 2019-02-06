d323410d1f021e2d441e75bb1041c83920cdf339
Matan Davidi <davidi.matan@gmail.com>
2019-02-06 13:36:46 +0100

    Added javadoc comments

--- src/ConcurrentServerManager.java
+++ src/ConcurrentServerManager.java
@@ -1,14 +1,19 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * The ConcurrentServerManager class manages simultaneous clients connecting to the server by creating a unique thread
+ * for each client
+ */
 public class ConcurrentServerManager {
 
     /**
-     * Metodo
+     * Method to verify the correct functioning of the program
      *
-     * @param args
+     * @param args the arguments passed via command line
      */
     public static void main(String[] args) {
 
@@ -16,17 +21,23 @@ public class ConcurrentServerManager {
 
         try {
 
+            //Create the server
             ServerSocket server = new ServerSocket(PORT);
             System.out.println("Listening on port " + PORT);
 
             while (true) {
 
+                //Accept a connection from the client
                 Socket client = server.accept();
                 System.out.println("Connection accepted from " + client.getRemoteSocketAddress());
                 (new ConcurrentWebServer(client, Paths.get("C:", "www"))).start();
 
             }
+                //Specify a hard-coded folder where the server is going to look for html files
+                //Create a server thread
 
+            }
+        //If there's a problem with the server's creation
         } catch (IOException ioe) {
 
             System.out.println("Could not bind port " + PORT + " to a server");
--- src/ConcurrentWebServer.java
+++ src/ConcurrentWebServer.java
@@ -3,14 +3,26 @@ import java.net.Socket;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * The ConcurrentWebServer class is a subclass of Thread and it manages requests coming from a client offering to
+ * display a certain html page based on the request as the response
+ */
 public class ConcurrentWebServer extends Thread {
 
+    /**
+     * The client that is connected to this server
+     */
     private Socket client;
 
+    /**
+     * The folder in which the server has to look for html files to offer
+     */
     private Path htdocsFolder;
 
     /**
-     * The class ConcurrentWebServer is a subclass of Thread and it allows to manage the
+     * Instantiates new objects of type ConcurrentWebServer with a default value for the field htdocsFolder
+     *
+     * @param client the client that is connected to this server
      */
     public ConcurrentWebServer(Socket client) {
 
@@ -18,6 +30,13 @@ public class ConcurrentWebServer extends Thread {
 
     }
 
+    /**
+     * Instantiates new objects of type ConcurrentWebServer allowing to specify a value for the fields client and
+     * htdocsFolder
+     *
+     * @param client       the client that is connected to this server
+     * @param htdocsFolder the folder in which the server has to look for html files to offer
+     */
     public ConcurrentWebServer(Socket client, Path htdocsFolder) {
 
         this.client = client;
@@ -25,6 +44,9 @@ public class ConcurrentWebServer extends Thread {
 
     }
 
+    /**
+     * Reads the client's request and replies with an appropriate html page
+     */
     @Override
     public void run() {
 
@@ -32,18 +54,25 @@ public class ConcurrentWebServer extends Thread {
 
             try {
 
+                //Get the client's input and output streams
                 BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 BufferedWriter out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+                //Read the client's request
                 String request = in.readLine();
 
                 if (request != null && request.length() > 0) {
 
                     WebPages pages = new WebPages(in, out);
+                //Create a new object of type ClientRequestManager
 
+                //If the request exists, meaning that the client actually sent something and is valid
+                    //Reads the resource the client is looking for (if any)
                     Path requestPath = Paths.get(getRequestFile(request.trim()));
 
                     try {
 
+                        //If the requested resource is empty, change it to index.html
                         if (requestPath.toString().equalsIgnoreCase("")) {
 
                             pages.printHelloWorld();
@@ -54,19 +83,25 @@ public class ConcurrentWebServer extends Thread {
 
                         }
 
+                        //Print requested html page
+                        //If the resource was not found
                     } catch (FileNotFoundException fnfe) {
 
                         pages.print404ErrorPage(Paths.get(htdocsFolder.toString(), requestPath.toString()).toString());
+                        //Print a 404 error page
 
                     }
 
+                    //If the request wasn't validated print a Bad Request page
                 }
 
+                //Close connection resources
                 out.close();
                 in.close();
 
             } catch (IOException ioe) {
 
+                //If there is a problem with the input/output from/to the client
                 System.out.println("Unable to read and/or write from/to the client.");
 
             }
@@ -75,6 +110,12 @@ public class ConcurrentWebServer extends Thread {
 
     }
 
+    /**
+     * Gets the requested resource from a request string
+     *
+     * @param request the string from which to extrapolate the requested resource
+     * @return a String containing the name of the requested resource
+     */
     public String getRequestFile(String request) {
 
         if (request.equalsIgnoreCase("GET / HTTP/1.1")) {

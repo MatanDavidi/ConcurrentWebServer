695fc9c728389f39bb1ce78dc51a25858df7f250
Matan Davidi <davidi.matan@gmail.com>
2019-02-11 16:24:23 +0100

    Copied IntelliJ source files over to NetBeans project

--- NetBeans/ConcurrentWebServer/src/ClientResponseManager.java
+++ NetBeans/ConcurrentWebServer/src/ClientResponseManager.java
@@ -1,4 +1,3 @@
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -7,8 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * The ClientResponseManager class contains useful methods to write resources to
- * an output stream.
+ * The ClientResponseManager class contains useful methods to write resources to an output stream.
  */
 public class ClientResponseManager {
 
@@ -18,7 +16,7 @@ public class ClientResponseManager {
     private OutputStream out;
 
     /**
-     * Istantiates new objects of type ClientResponseManager.
+     * Instantiates new objects of type ClientResponseManager.
      *
      * @param out the output stream to write to.
      */
@@ -31,52 +29,69 @@ public class ClientResponseManager {
     /**
      * Writes a 400 error page to the output stream.
      *
-     * @throws IOException in case there's an error while writing to the output
-     * stream.
+     * @throws IOException in case there's an error while writing to the output stream.
      */
     public void return400ErrorPage() throws IOException {
 
-        returnFile(Paths.get("/pages", "errors", "4xx", "400.html"));
+        returnFile(Paths.get("pages", "errors", "4xx", "400.html"));
 
     }
 
     /**
      * Writes a 404 error page to the output stream.
      *
-     * @throws IOException in case there's an error while writing to the output
-     * stream.
+     * @throws IOException in case there's an error while writing to the output stream.
      */
     public void return404ErrorPage(/*String file*/) throws IOException {
-        
-        returnFile(Paths.get("/pages", "errors", "4xx", "404.html"));
+
+        returnFile(Paths.get("pages", "errors", "4xx", "404.html"));
 
     }
 
     /**
      * Writes the HTTP header to the output stream.
      *
-     * @throws IOException in case there's an error while writing to the output
-     * stream.
+     * @throws IOException in case there's an error while writing to the output stream.
      */
     private void returnHttpHeader() throws IOException {
 
-        out.write("HTTP/1.1 200 OK\r\n".getBytes());
-        out.write("\r\n".getBytes());
+        returnHttpHeader("HTTP/1.1 200 OK\r\n", "");
+
 
     }
 
     /**
-     * Writes the HTTP header to the output stream allowing to specify a certain
-     * file type.
+     * Writes the HTTP header to the output stream allowing to specify a certain file type.
      *
      * @param fileType the type of the file to write to the output stream.
-     * @throws IOException in case there's an error while writing to the output
-     * streams.
+     * @throws IOException in case there's an error while writing to the output streams.
      */
     private void returnHttpHeader(String fileType) throws IOException {
 
-        out.write("HTTP/1.1 200 OK\r\n".getBytes());
-        out.write((fileType + "\r\n").getBytes());
+        returnHttpHeader("HTTP/1.1 200 OK", fileType);
+
+
+    }
+
+    /**
+     * Writes the HTTP header to the output stream allowing to specify a certain response code and file type.
+     *
+     * @param response the response code to write to the output stream.
+     * @param fileType the type of the file to write to the output stream.
+     * @throws IOException in case there's an error while writing to the output streams.
+     */
+    private void returnHttpHeader(String response, String fileType) throws IOException {
+
+        if (!response.endsWith("\r\n")) {
+            response += "\r\n";
+        }
+
+        if (!fileType.equals("") && !fileType.endsWith("\r\n")) {
+            fileType += "\r\n";
+        }
+
+        out.write(response.getBytes());
+        out.write(("Content-Type: " + fileType).getBytes());
         out.write("\r\n".getBytes());
 
     }
@@ -85,8 +100,7 @@ public class ClientResponseManager {
      * Writes the contents of a file to the output stream.
      *
      * @param url the path to the file of which to write the contents.
-     * @throws IOException in case there's an error while reading the file's
-     * contents or writing to the output stream.
+     * @throws IOException in case there's an error while reading the file's contents or writing to the output stream.
      */
     public void returnFile(Path url) throws IOException {
 
@@ -94,24 +108,11 @@ public class ClientResponseManager {
         if (Files.exists(url) && !Files.notExists(url)) {
 
             //Write a different file type depending on the file's extension
-            String lowerUrl = url.toString().toLowerCase();
-            if (lowerUrl.endsWith(".html")) {
+            String lowerUrl = url.toString().trim().toLowerCase();
+            String fileType = getFileType(lowerUrl);
 
-                returnHttpHeader("text/html");
-
-            } else if (lowerUrl.endsWith(".css")) {
-
-                returnHttpHeader("text/css");
-
-            } else if (lowerUrl.endsWith(".js")) {
-
-                returnHttpHeader("application/javascript");
-
-            } else {
-
-                returnHttpHeader();
-
-            }
+            //Write the HTTP header e.g. HTTP/1.1 200 OK
+            returnHttpHeader(fileType);
 
             //Read the file's data
             byte[] fileContent = Files.readAllBytes(url);
@@ -125,6 +126,46 @@ public class ClientResponseManager {
             throw new FileNotFoundException("Unable to find file " + url.toString());
 
         }
+
+    }
+
+    /**
+     * Gets the MIME file type based on the file's extension.
+     *
+     * @param lowerUrl a String containing the file to analyze
+     * @return a String containing the MIME file type e.g. text/html
+     */
+    private String getFileType(String lowerUrl) {
+
+        String re = "";
+
+        if (lowerUrl.endsWith(".html")) {
+
+            re = "text/html";
+
+        } else if (lowerUrl.endsWith(".css")) {
+
+            re = "text/css";
+
+        } else if (lowerUrl.endsWith(".js")) {
+
+            re = "application/javascript";
+
+        } else if (lowerUrl.endsWith(".jpg") || lowerUrl.endsWith(".jpeg")) {
+
+            re = "image/jpeg";
+
+        } else if (lowerUrl.endsWith(".png")) {
+
+            re = "image/png";
+
+        } else if (lowerUrl.endsWith(".bmp")) {
+
+            re = "image/bmp";
+
+        }
+
+        return re;
 
     }
 
